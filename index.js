e36739c743c123d0bbeb8c5270093cb4d63af619
Steve Lacy <me@slacy.me>
2014-10-17 13:42:58 -0700

    gulp-git

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ var config = {
   port: 3000,
   repo: 'git@github.com:stevelacy/git-hook2.git'
 };
-var port = 3000;
 
 
-
-var start = function(){
+var start = function() {
   var cmd = 'pm2 restart 0';
   exec(cmd, function(err, stdout, stderr){
-    if (err){
-      // cry
-
-    }
+    console.log(err, stdout, stderr);
   });
 };
 
@@ -45,7 +40,11 @@ var server = http.createServer(function(req, res) {
       res.writeHead(200, {
         'Content-type': 'text/html'
       });
+      return res.end();
     }
+    res.writeHead(401, {
+      'Content-type': 'text/html'
+    });
     return res.end();
   });
 
