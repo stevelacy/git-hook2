791a14e6e86be5263ee87be46aad61b1b9d50b19
Steve Lacy <me@slacy.me>
2014-10-17 12:40:57 -0700

    gulp-git

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+
+
+
+
+gulp.task('git', function(){
+  return gulp.src('.')
+    .pipe(git.add({args: '-A'}))
+    .pipe(git.commit('gulp-git'))
+    .on('end', function(){
+      git.push('origin', 'master', function(err){
+        if (err){
+          console.log(err);
+        }
+      });
+    });
+});
+
+
+gulp.task('default', ['git']);
